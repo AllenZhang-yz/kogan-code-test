e2947165622623fde6c9d84eec66eecc41a45495
Allen Zhang <allen.zhang018@gmail.com>
2020-11-11 23:13:44 +1100

    add deployment info

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.static(path.join('build')));
 
 app.get('/api/get-products', async (req, res) => {
   let page = 1;
@@ -20,6 +23,10 @@ app.get('/api/get-products', async (req, res) => {
     }
   } while (fetchNext);
   res.status(200).json(productsData);
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 4000;
